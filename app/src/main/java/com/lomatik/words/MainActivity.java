9a4f8761e19ed8007bac1148f7713a813b31f751
lomatik <ralenko2012@gmail.com>
2020-12-02 19:40:06 +0200

    test timer writed

--- app/src/main/java/com/lomatik/words/MainActivity.java
+++ app/src/main/java/com/lomatik/words/MainActivity.java
@@ -3,12 +3,55 @@ package com.lomatik.words;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    TextView textView;
+    CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editTextNumber);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int seconds = Integer.valueOf(editText.getText().toString());
+                countDownTimer = new CountDownTimer(seconds * 1000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        NumberFormat f = new DecimalFormat("00");
+                        long hour = (l / 3600000) % 24;
+                        long min = (l / 60000) % 60;
+                        long sec = (l / 1000) % 60;
+                        textView.setText(f.format(hour) + ":" + f.format(min) + ":" + f.format(sec));
+                        if (sec == 3) {
+                            Toast.makeText(getApplicationContext(),"stop",Toast.LENGTH_SHORT).show();
+                            cancel();
+                        }
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        textView.setText("Finished!");
+                    }
+                };
+                countDownTimer.start();
+            }
+        });
     }
 }
